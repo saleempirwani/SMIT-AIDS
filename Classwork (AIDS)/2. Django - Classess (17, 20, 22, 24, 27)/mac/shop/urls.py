7a6a07241a9da554befcc0abf69fa8ce6116fee0
Saleem Raza <salimraza3465@gmail.com>
2021-03-20 23:11:18 +0500

    rename && add class 30

--- Classwork (AIDS)/2. Django - Classess (17, 20, 22, 24, 27)/mac/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="shopHome"),
-    path('product/', views.product, name="shopProduct"),
-    path('about/', views.about, name="shopAbout"),
-]
